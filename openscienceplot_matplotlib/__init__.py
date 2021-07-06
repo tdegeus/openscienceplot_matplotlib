92a136a499030841b2ad34f89e5a6f5d30da91fd
Tom de Geus <tom@geus.me>
2021-07-06 09:48:25 +0200

    Updating documentation

--- openscienceplot_matplotlib/__init__.py
+++ openscienceplot_matplotlib/__init__.py
@@ -22,40 +22,23 @@ import operator
 import functools
 from functools import singledispatch
 
-
-def info():
+def _interpret(handle):
     r'''
-Return basic library information.
-    '''
-
-    return [
-        'openscienceplot_matplotlib={0:s}'.format(__version__),
-        'matplotlib={0:s}'.format(matplotlib.__version__)]
-
-
-@singledispatch
-def dump(handle):
-    r'''
+(Private function)
 Return to be stored data from a handle.
 
 Essential attributes are outputted as well, as follows:
 
-*   matplotlib.lines.Line2D ('artist'): 'color', `linestyle', 'marker', 'label'.
-*   matplotlib.container.ErrorbarContainer ('artist'): 'color', `linestyle', 'marker', 'label'.
+*   matplotlib.lines.Line2D ('artist'):
+    'color', 'linestyle', 'marker', 'label'.
+
+*   matplotlib.container.ErrorbarContainer ('artist'):
+    'color', 'linestyle', 'marker', 'label'.
     Warning: for now saved as matplotlib.lines.Line2D, without error-bars.
 
-:argument:
-
-    **handle**
-        The object's handle.
-
-:return:
-
-    **data** (``<numpy.ndarray>``)
-        The data.
-
-    **attributes** (``<dict>``)
-        Essential attributes.
+:param matplotlib_object handle: The object's handle.
+:return: (data, attributes)
+:rtype: numpy.ndarray, dict
     '''
 
     import warnings
@@ -88,24 +71,54 @@ Essential attributes are outputted as well, as follows:
     raise IOError('Unknown handle. Store manually, or consider filing a bug-report.')
 
 
-@dump.register(h5py.File)
-def _(f, key, handle):
+def info():
     r'''
-Save plot and attributes to HDF5-file.
-
-:arguments:
-
-    **f** (``<h5py.File>``)
-        Opened HDF5 file.
-
-    **key** (``<str>``)
-        Name of the dataset to which to write.
-
-    **handle**
-        The handle to write.
+Return basic library information.
     '''
 
-    data, attributes = dump(handle)
+    return [
+        'openscienceplot_matplotlib={0:s}'.format(__version__),
+        'matplotlib={0:s}'.format(matplotlib.__version__)]
+
+
+@singledispatch
+def dump():
+    r'''
+Save plot and attributes.
+
+Essential attributes are stored as well, as follows:
+
+*   matplotlib.lines.Line2D ('artist'):
+    'color', 'linestyle', 'marker', 'label'.
+
+*   matplotlib.container.ErrorbarContainer ('artist'):
+    'color', 'linestyle', 'marker', 'label'.
+    Warning: for now saved as matplotlib.lines.Line2D, without error-bars.
+
+:param f: Opened HDF5 file.
+:type f: h5py.File, dict.
+
+:param str key:
+    Name of the dataset to which to write.
+    Path separated by "/", or specified as list.
+
+:param matplotlib_object handle: The handle to write.
+
+:param kwargs: Additional attributes to add (e.g. linestyle).
+
+:return: f
+    '''
+    pass
+
+
+@dump.register(h5py.File)
+def _(f, key, handle, **kwargs):
+
+    data, attributes = _interpret(handle)
+
+    for key in kwargs:
+        assert key not in attributes
+        attributes[key] = kwargs[key]
 
     dset = f.create_dataset(key, data.shape, dtype=data.dtype)
     dset[:] = data
@@ -115,32 +128,17 @@ Save plot and attributes to HDF5-file.
 
 
 @dump.register(dict)
-def _(f, key, handle):
-    r'''
-Save plot and attributes to a dictionary.
-
-:arguments:
-
-    **f** (``<dict>``)
-        Dictionary.
-
-    **key** (``<str>`` | ``<list>``)
-        Name of the dataset to which to write.
-        Path separated by "/", or as list.
-
-    **handle**
-        The handle to write.
-
-:returns:
-
-    Dictionary.
-    '''
+def _(f, key, handle, **kwargs):
 
     if type(key) == str:
         key = list(filter(None, key.split("/")))
 
-    data, attributes = dump(handle)
+    data, attributes = _interpret(handle)
     attributes["data"] = data
+
+    for key in kwargs:
+        assert key not in attributes
+        attributes[key] = kwargs[key]
 
     functools.reduce(operator.getitem, key[:-1], f)[key[-1]] = attributes
 
@@ -149,23 +147,10 @@ def restore_h5py(data, key, axis=None):
     r'''
 Restore plot from HDF5-file.
 
-:arguments:
-
-    **data** (``h5py.File``)
-        Opened HDF5 file.
-
-    **key** (``<str>``)
-        Name of the dataset from which to read.
-
-:options:
-
-    **axis** ([``plt.gca()``] | ...)
-        Specify the axis on which to plot.
-
-:returns:
-
-    **handle**
-        The handle of the created plot.
+:param h5py.File data: Opened HDF5 file.
+:param str key: Name of the dataset from which to read.
+:param matplotlib_object axis: The axis on which to plot (default ``plt.gca()``).
+:return: The handle of the created plot.
     '''
 
     if axis is None:
